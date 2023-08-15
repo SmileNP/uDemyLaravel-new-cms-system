022f8a0aa5e496ad6b0e41a52bff018f0b76aae5
SmileNP <smile.bestybay@nabepero.co.jp>
2023-08-14 23:22:31 -0700

    seeding db

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+            'user_id'=>\App\Models\User::factory(),
+            'title' => $this->faker->sentence(),
+            'post_image' => $this->faker->imageUrl('900', '300'),
+            'body' => $this->faker->paragraph(),
+        ];
+    }
+}
