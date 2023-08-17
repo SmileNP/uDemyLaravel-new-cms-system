e93c16b497c203f91a160e127c361e4f808959be
SmileNP <smile.bestybay@nabepero.co.jp>
2023-08-16 22:42:14 -0700

    complete create and read

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -1,5 +1,59 @@
 <x-admin-master>
     @section('content')
-        Roles
+        <div class="row">
+            <div class="col-sm-3">
+                <form action="{{route('roles.store')}}" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" id="name" name="name"
+                               class="form-control @error('name') is-invalid @enderror">
+                    </div>
+                    <div>
+                        @error('name')
+                        <span><strong>{{$message}}</strong></span>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">Create</button>
+                </form>
+            </div>
+
+            <div class="col-sm-9">
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-primary">Posts Table</h6>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <td>Name</td>
+                                    <th>Slug</th>
+                                </tr>
+                                </thead>
+                                <tfoot>
+                                <tr>
+                                    <th>Id</th>
+                                    <td>Name</td>
+                                    <th>Slug</th>
+                                </tr>
+                                </tfoot>
+                                <tbody>
+                                @foreach($roles as $role)
+                                    <tr>
+                                        <td>{{$role->id}}</td>
+                                        <td>{{$role->name}}</td>
+                                        <td>{{$role->slug}}</td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     @endsection
 </x-admin-master>
--- routes/web/roles.php
+++ routes/web/roles.php
@@ -2,3 +2,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/roles', [App\Http\Controllers\RoleController::class, 'index'])->name('roles.index');
+Route::post('/roles', [App\Http\Controllers\RoleController::class, 'store'])->name('roles.store');
