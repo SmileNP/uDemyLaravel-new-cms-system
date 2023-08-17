5ebdc40f759cf8bb73fb45e77ee2bb74477299f5
SmileNP <smile.bestybay@nabepero.co.jp>
2023-08-16 17:06:08 -0700

    add role middleware

--- resources/views/components/admin-master.blade.php
+++ resources/views/components/admin-master.blade.php
@@ -61,7 +61,9 @@
         <!-- Nav Item - Pages Collapse Menu -->
         <x-admin-sidebar-posts-links></x-admin-sidebar-posts-links>
 
-        <x-admin-sidebar-users-links></x-admin-sidebar-users-links>
+        @if(auth()->user()->userHasRole('Admin'))
+            <x-admin-sidebar-users-links></x-admin-sidebar-users-links>
+        @endif
 
         <!-- Nav Item - Utilities Collapse Menu -->
         <li class="nav-item">
