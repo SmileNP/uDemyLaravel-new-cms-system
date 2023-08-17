7074472d88495032b2fec995efa06b9405af6386
SmileNP <smile.bestybay@nabepero.co.jp>
2023-08-17 00:26:36 -0700

    attach detach permissions

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Role;
+use App\Models\Permission;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
@@ -23,12 +24,43 @@ class RoleController extends Controller
         ]);
         Role::create([
             'name'=>Str::ucfirst(request('name')),
-            'slug'=>Str::of(Str::lower(request('name')))->slug('-'),
+            'slug'=>Str::of(Str::lower(request('name')))->slug(),
         ]);
 
         return back();
     }
 
+    public function edit(Role $role)
+    {
+        return view('admin.roles.edit', [
+            'role'=>$role,
+            'permissions'=>Permission::all(),
+        ]);
+    }
+
+    public function update(Role $role)
+    {
+        $role->name = Str::ucfirst(request('name'));
+        $role->slug = Str::of(Str::lower(request('name')))->slug();
+        if($role->isDirty('name')) {
+            session()->flash('role-updated', 'Role updated: '.$role->name);
+            $role->save();
+        } else {
+            session()->flash('role-updated', 'Nothing has been updated');
+        }
+        return back();
+    }
+
+    public function attach_permission(Role $role)
+    {
+        $role->permissions()->attach(request('permission'));
+        return back();
+    }
+    public function detach_permission(Role $role)
+    {
+        $role->permissions()->detach(request('permission'));
+        return back();
+    }
     public function destroy(Role $role)
     {
         $role->delete();
